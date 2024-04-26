30a629a6ee326ec54edeeeab2f233ffabf443cdb
yeswanthkumargurram <yeswanthkumar.gurram@gmail.com>
2024-04-26 23:56:42 +0530

    Initial commit

---
+++ src/main/java/com/example/productservice_fakestoreapi/ProductServiceFakeStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.productservice_fakestoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceFakeStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceFakeStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/productservice_fakestoreapi/ProductServiceFakeStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productservice_fakestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceFakeStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
